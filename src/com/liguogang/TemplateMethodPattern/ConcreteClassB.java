2f3908d33ab7fa84ad490578273dcfebdbeb70fc
lee58 <952051088@qq.com>
2021-02-15 10:53:57 +0800

    模板方法模式

---
+++ src/com/liguogang/TemplateMethodPattern/ConcreteClassB.java
@@ -0,0 +1,12 @@
+package com.liguogang.TemplateMethodPattern;
+
+/**
+ * @author 大都督
+ * @create 2021/2/15
+ */
+public class ConcreteClassB extends AbstractClass {
+    @Override
+    protected void step2() {
+        System.out.println("ConcreteClassB step2");
+    }
+}
